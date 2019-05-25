8d4499de5cbe7353ab5bf83620d9f2d96faaf99f
Alikhan Gatsalov <alihan_g5@mail.ru>
2019-05-25 20:40:38 +0300

    init

---
+++ index.js
@@ -0,0 +1,63 @@
+(function(){let btns = document.querySelectorAll('.slider__arrow');
+
+function clickEventHandler(e){
+  move(e.target.dataset.direction);
+}
+
+function addClickEvent(){
+  for(let i = 0; i < btns.length; i++){
+    btns[i].addEventListener('click', clickEventHandler)
+  }
+}
+
+function keypadEventHandler(e){
+    if(e.keyCode === 37){
+        move('left');
+    }else if(e.keyCode === 39){
+        move('right');
+    }
+}
+function addKeypadEvent(){
+  addEventListener('keyup',keypadEventHandler)
+}
+
+
+function move(direction){
+
+    let slide = document.querySelector('.focus'),
+        shiftPosition = slide.clientWidth,
+        nextSibling = slide.nextElementSibling,
+        prevSibling = slide.previousElementSibling,
+        parent = slide.parentNode,
+        currentPosition = parent.offsetLeft;
+    
+    parent.addEventListener('transitionend',(e)=>{
+      addEvents()
+    })
+    if(direction == 'left' && nextSibling){
+        clearEvents();
+        parent.style.left = `${ currentPosition -= shiftPosition}px`;
+        nextSibling.classList.add('focus');
+        slide.classList.remove('focus');
+    }else if(direction == 'right' && prevSibling){
+        clearEvents();
+        parent.style.left = `${ currentPosition += shiftPosition}px`;
+        prevSibling.classList.add('focus');
+        slide.classList.remove('focus');
+    }
+}
+
+
+function clearEvents (){
+  for(let i = 0; i < btns.length; i++){
+    btns[i].removeEventListener('click', clickEventHandler)
+  }
+  removeEventListener('keyup', keypadEventHandler)
+}
+
+function addEvents (){
+  addKeypadEvent();
+  addClickEvent();
+}
+addEvents();
+})()
